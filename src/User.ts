186d6d7d52fb60bdbbebbfbc66aa71b92df00d18
jverat <jverat@unal.edu.co>
2020-06-12 20:18:02 -0500

    a bunch of stuff

---
+++ src/User.ts
@@ -0,0 +1,10 @@
+type User = {
+    username:string,
+    password:string,
+    email:string
+}
+
+function newUser(username:string, password:string, email:string): void {
+
+}
+export {User};
--- src/routes/LogInRoutes.ts
+++ src/routes/LogInRoutes.ts
@@ -1,2 +1,16 @@
-import {Router, Response, Request} from "express";
+import express, {Request, Response, Router} from "express";
+import {User} from "../User";
 
+//const loginRouter = Router();
+const app = express();
+
+let u: User = {username:"pepito", password:"perez", email:"pepito@perez.com"}
+let users: User[] = [u];
+
+
+app.route("/api").get((req: Request, res: Response)=>{ res.json("miapi"); })
+
+//Request = username
+app.get("/api/user/:username", (req: Request, res: Response)=>{
+    res.json(users.find(user => user.username === req.params.username));
+})
