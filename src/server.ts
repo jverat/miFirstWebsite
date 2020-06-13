9478af3fc02e7a2e88321e39063502438b939b18
jverat <jverat@unal.edu.co>
2020-06-12 20:41:27 -0500

    Server initialization

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+class Server {
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+    }
+
+    config() {
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    routes() {
+
+    }
+
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port', this.app.get('port'));
+        });
+    }
+}
